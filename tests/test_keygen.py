1007189c7cbf8ec6c338a5dc2f96dfb208f115b0
Sokinin <jkkinin360@gmail.com>
2025-12-14 15:15:26 +0700

    New change from scratch

--- tests/test_keygen.py
+++
@@ -1,8 +0,0 @@
-from src.crypto import keygen
-
-
-def test_generate_keys():
-    xpriv, xpub = keygen.generate_x25519_keypair()
-    epriv, epub = keygen.generate_ed25519_keypair()
-    assert xpriv is not None and xpub is not None
-    assert epriv is not None and epub is not None
